a5c14a5fc411ec49efbfcc166731cc9513511784
Mark <DreynDev@gmail.com>
2025-02-10 15:18:58 +0300

    cart btn

--- src/components/CartEmpty/CartEmpty.jsx
+++ src/components/CartEmpty/CartEmpty.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './CartEmpty.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function CartEmpty() {
   return (
-    <>
-      <h1 className={styles.root}>Cart is empty :(</h1>;
-    </>
+    <div className={styles.root}>
+      <h1>Cart is empty :(</h1>
+      <Link to="/" className="button button--outline button--add go-back-btn">
+        <span>Повернутись назад</span>
+      </Link>
+    </div>
   );
 }
